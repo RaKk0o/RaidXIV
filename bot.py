035309260dd0309f21faed90ee61ff54875ede72
Clémence Faligot <75119672+RaKk0o@users.noreply.github.com>
2024-06-13 14:51:05 +0200

    / command

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ intents = nextcord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents)
 
 # Dictionary to store event information
 events = {}
