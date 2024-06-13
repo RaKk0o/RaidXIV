4bcd5080de896df8716fd708648c3a64569a12b3
Clémence Faligot <75119672+RaKk0o@users.noreply.github.com>
2024-06-13 15:41:40 +0200

    run test bot

--- test_bot.py
+++ test_bot.py
@@ -1,20 +1,42 @@
 import os
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
+from discord import app_commands
+import logging
 
-load_dotenv()  # Charger les variables d'environnement depuis le fichier .env
+# Configurer le logging
+logging.basicConfig(level=logging.INFO)
 
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot connecté en tant que {bot.user}')
-    await bot.close()  # Arrêter le bot après la connexion
+    logging.info(f'We have logged in as {bot.user}')
+    await bot.tree.sync()
 
-# Lire le token depuis les variables d'environnement
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def on_ready(self):
+        logging.info(f'We have logged in as {self.user}')
+        await self.tree.sync()
+
+client = MyClient()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@client.tree.command(name="hello", description="Says hello!")
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hello!", ephemeral=True)
+
+@client.tree.command(name="ping", description="Pong!")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
+
 token = os.getenv('DISCORD_TOKEN')
-bot.run(token)
+client.run(token)
