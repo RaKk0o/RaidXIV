2ca8f6744ca71fca4ca844c5229522b86062d67e
Clémence Faligot <75119672+RaKk0o@users.noreply.github.com>
2024-06-17 13:26:41 +0200

    add logo

--- commands/__init__.py
+++
@@ -1,4 +0,0 @@
-# commands/__init__.py
-from .create_event import create_event_command
-from .edit_event import edit_event_command
-from .buttons import PresenceButton, AbsenceButton, MaybeButton, ReplacementButton
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-# utils/__init__.py
-from .helpers import validate_date, validate_time, CreateEventSelect, CreateEventView, update_event_message
